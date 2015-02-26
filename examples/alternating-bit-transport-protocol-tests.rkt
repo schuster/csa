a16eaffd0832ceef4188f7972d725f2d30bd3459
Jonathan Schuster <schuster@ccs.neu.edu>
2015-02-26 12:08:56 -0500

    Add ABTP

---
+++ examples/alternating-bit-transport-protocol-tests.rkt
@@ -0,0 +1,116 @@
+#lang racket
+
+(require "alternating-bit-transport-protocol.rkt")
+(require rackunit)
+(require asyncunit)
+(require racket/async-channel)
+
+;; ---------------------------------------------------------------------------------------------------
+;; Sender tests
+
+(define (make-sender)
+  (define receiver-channel (make-async-channel))
+  (define status-channel (make-async-channel))
+  (match-define (list message-to-send queued-message close-session from-receiver)
+    (Sender receiver-channel 80 status-channel))
+  (values message-to-send
+          queued-message
+          close-session
+          from-receiver
+          receiver-channel
+          status-channel))
+
+(define (goto-Connected from-receiver status-channel)
+  (async-channel-put from-receiver (SynAck))
+  (check-unicast-match status-channel (list 'Connected _ _)))
+
+(test-case
+ "Initial connection time-out"
+ (match-define-values (_ _ _ _ _ status-channel) (make-sender))
+ (sleep 3)
+ (check-unicast status-channel (ConnectFailed)))
+
+(test-case
+ "Initial connection success"
+
+ (match-define-values (_ _ _ from-receiver _ status-channel) (make-sender))
+ (async-channel-put from-receiver (SynAck))
+ (check-unicast-match status-channel (list 'Connected _ _))
+ ;; void here just prevents output when running the test
+ (void))
+
+(test-case
+ "Timeout on message send, with multiples sent"
+ (match-define-values (message-to-send _ _ from-receiver to-receiver status-channel)
+   (make-sender))
+ (goto-Connected from-receiver status-channel)
+ (define req-response (make-async-channel))
+ (async-channel-put message-to-send req-response)
+ (check-unicast req-response (Queued))
+ (sleep 9)
+ (check-unicast status-channel (ErrorClosed))
+ (check-unicast-match to-receiver _) ; ignore the Syn
+ (check-unicast-match to-receiver (list 'Message _ (list 'Seq0) (list 'Unit)))
+ (check-unicast-match to-receiver (list 'Message _ (list 'Seq0) (list 'Unit)))
+ (check-unicast-match to-receiver (list 'Message _ (list 'Seq0) (list 'Unit))))
+
+;; ---------------------------------------------------------------------------------------------------
+;; Receiver tests
+
+(test-case
+ "New receiver broadcasts an Ack1 periodically"
+ (define to-net (make-async-channel))
+ (define to-app (make-async-channel))
+ (match-define  (list _) (Receiver to-net to-app))
+ (check-unicast to-net (SynAck))
+ (check-unicast to-net (Ack1) #:timeout 1.1)
+ (check-unicast to-net (Ack1) #:timeout 1.1))
+
+(test-case
+ "Sending Seq0 results in an Ack0, eventually"
+ (define to-net (make-async-channel))
+ (define to-app (make-async-channel))
+ (match-define (list from-sender) (Receiver to-net to-app))
+ (async-channel-put from-sender (Message 80 (Seq0) (Unit)))
+ (define first-response (check-unicast-match to-net response #:result response))
+ (unless (equal? first-response (Ack0))
+   (check-unicast to-net (Ack0))))
+
+(test-case
+ "Sending Seq1 doesn't change the Ack"
+ (define to-net (make-async-channel))
+ (define to-app (make-async-channel))
+ (match-define (list from-sender) (Receiver to-net to-app))
+ (check-unicast to-net (SynAck))
+ (async-channel-put from-sender (Message 80 (Seq1) (Unit)))
+ (check-unicast to-net (Ack1) #:timeout 1.1)
+ (check-unicast to-net (Ack1) #:timeout 1.1))
+
+;; ---------------------------------------------------------------------------------------------------
+;; Manager tests
+
+;; Check that we can request a new session, send a message, get a "queued" back, send "close", get
+;; "closing", "closed" back
+
+(test-case
+ "Conversation through ABTP service"
+ (define app-channel (make-async-channel))
+ (define send-response-channel (make-async-channel))
+ (define connection-status-channel (make-async-channel))
+ (match-define (list start-sender from-net)
+   (Manager app-channel (make-async-channel)))
+ (async-channel-put start-sender
+                    (Connect from-net
+                             80
+                             connection-status-channel))
+ (match-define (list send-channel close-channel)
+   (check-unicast-match connection-status-channel
+                        (list 'Connected send-channel close-channel)
+                        #:result (list send-channel close-channel)))
+ (async-channel-put send-channel send-response-channel)
+ (check-unicast send-response-channel (Queued))
+ (sleep .1)
+ (async-channel-put close-channel (Unit))
+ (check-unicast connection-status-channel (Closed))
+ (async-channel-put send-channel send-response-channel)
+ (check-unicast send-response-channel (WriteFailed)))
---
+++ examples/alternating-bit-transport-protocol.rkt
@@ -0,0 +1,322 @@
+#lang csa
+
+;; Implements actors and specifications for a variant of the alternating bit protocol, with a two-way
+;; handshake for both the beginning and end of a session.
+
+(provide Sender
+         Receiver
+         Manager
+
+         Connected
+         ConnectFailed
+         Closed
+         ErrorClosed
+
+         Syn
+         Message
+         Fin
+         Seq0
+         Seq1
+
+         SynAck
+         Ack0
+         Ack1
+         FinAck
+
+         Unit
+
+         Queued
+         WriteFailed
+
+         Connect)
+
+;; ---------------------------------------------------------------------------------------------------
+
+;; Wire-level messages
+(define-variant-type SenderMessage
+  [Syn Nat (Channel ReceiverMessage)]
+  [Message Nat SequenceNumber Unit]
+  [Fin Nat])
+(define-variant-type ReceiverMessage [SynAck] [Ack0] [Ack1] [FinAck])
+
+(define-variant-type SequenceNumber [Seq0] [Seq1])
+
+(define-variant-type AttemptCount
+  [OneAttempt]
+  [TwoAttempts]
+  [ThreeAttempts])
+
+;; Client-level messages
+(define-variant-type WriteResponse (Queued) (WriteFailed))
+(define-variant-type Status
+  [Connected (Channel Unit) (Channel Unit)]
+  [ConnectFailed]
+  [Closed]
+  [ErrorClosed])
+
+(define-variant-type AppMessage
+  [AppMessage Nat ; port
+              Unit]) ; content
+
+(define-variant-type Connect
+   (Connect (Channel ReceiverMessage)
+            Nat ; port
+            (Channel Status)))
+
+;; Misc. types
+(define-variant-type Unit [Unit])
+
+(define-variant-type EntryList
+  [Null]
+  [Cons Entry EntryList])
+(define-variant-type Entry
+  [Entry Natural ; port
+         (Channel SenderMessage)])
+
+;; ---------------------------------------------------------------------------------------------------
+;; Actors
+
+(define (Receiver to-sender to-app)
+  (spawn-agent
+   (([from-sender SenderMessage])
+    (define-state (Respond0 [to-sender (ChannelOf ReceiverMessage)]
+                            ;; should be AppMessage
+                            [to-app (ChannelOf AppMessage)])
+      [from-sender (msg)
+        (case msg
+          [Syn (p) (goto Respond0 to-sender to-app)] ; ignore Syn
+          [Message (port seq content)
+           (case seq
+             [Seq0 () (goto Respond0 to-sender to-app)]
+             [Seq1 ()
+               (send to-app (AppMessage port content))
+               (send to-sender (Ack1))
+               (goto Respond1 to-sender to-app)])]
+          [Fin (p)
+            (send to-sender (FinAck))
+            (goto Closed)])]
+      [(timeout 1)
+       (send to-sender (Ack0))
+       (goto Respond0 to-sender to-app)])
+
+    (define-state (Respond1 [to-sender (ChannelOf ReceiverMessage)]
+                            [to-app (ChannelOf Unit)])
+      [from-sender (msg)
+        (case msg
+          [Syn (p) (goto Respond1 to-sender to-app)] ; ignore Syn
+          [Message (port seq content)
+           (case seq
+             [Seq0 ()
+               (send to-app (AppMessage port content))
+               (send to-sender (Ack0))
+               (goto Respond0 to-sender to-app)]
+             [Seq1 () (goto Respond1 to-sender to-app)])]
+          [Fin (p)
+            (send to-sender (FinAck))
+            (goto Closed)])]
+      [(timeout 1)
+       (send to-sender (Ack1))
+       (goto Respond1 to-sender to-app)])
+
+    (define-state (Closed)
+      [from-sender (msg) (goto Closed)])
+
+    (begin
+      (send to-sender (SynAck))
+      (goto Respond1 to-sender to-app)))
+
+   (list from-sender)))
+
+(define (Sender to-recvr receiver-port status)
+  (spawn-agent
+   (([write Unit] [queued-message Unit] [close Unit] [from-recvr ReceiverMessage])
+
+    (define-state
+      (SynSent [to-recvr (ChannelOf SenderMessage)]
+               [port Nat]
+               [status (ChannelOf Status)])
+      [from-recvr (m)
+        (case m
+          [SynAck ()
+            (send status (Connected write close))
+            (goto Ready (Seq0) to-recvr port status)]
+          [Ack1 () (goto-this-state)]
+          [Ack0 () (goto-this-state)]
+          [FinAck () (goto-this-state)])]
+      [(timeout 3)
+        (send status (ConnectFailed))
+        (goto ClosedState)])
+
+    (define-state (Ready [current-seq SequenceNumber]
+                         [to-recvr (ChannelOf SenderMessage)]
+                         [port Nat]
+                         [status (ChannelOf Status)])
+      [write (response)
+        (send queued-message (Unit))
+        (send response (Queued))
+        (goto Ready current-seq to-recvr port status)]
+      [queued-message (m)
+        (send to-recvr (Message port current-seq m))
+        (goto AwaitingAck current-seq m (OneAttempt) to-recvr port status)]
+      [close (c)
+        (send to-recvr (Fin port))
+        (goto Closing status)]
+      [from-recvr (m)
+        ;; can ignore all receiver messages in this state
+        (goto Ready current-seq to-recvr port status)])
+
+    (define-state (AwaitingAck [last-sent-seq SequenceNumber]
+                               [message Unit]
+                               [send-attempts AttemptCount]
+                               [to-recvr (ChannelOf SenderMessage)]
+                               [port Nat]
+                               [status (ChannelOf Status)])
+      [write (r)
+        (send queued-message (Unit))
+        (send r (Queued))
+        (goto-this-state)]
+      [close (c)
+        (send to-recvr (Fin port))
+        ;; BUG: forgot to add arguments to the Closing state here
+        (goto Closing status)]
+      [from-recvr (m)
+        (case m
+          [SynAck () (goto-this-state)]
+          [Ack1 ()
+            (case last-sent-seq
+              [Seq0 () (goto-this-state)]
+              [Seq1 () (goto Ready (Seq0) to-recvr port status)])]
+          [Ack0 ()
+            (case last-sent-seq
+              ;; BUG: Seq0 in the args to Ready should really be Seq1
+              [Seq0 () (goto Ready (Seq0) to-recvr port status)]
+              [Seq1 () (goto-this-state)])]
+          [FinAck () (goto-this-state)])]
+      [(timeout 3)
+       (case send-attempts
+         [OneAttempt ()
+           (send to-recvr (Message port last-sent-seq message))
+           (goto AwaitingAck last-sent-seq message (TwoAttempts) to-recvr port status)]
+         [TwoAttempts ()
+           (send to-recvr (Message port last-sent-seq message))
+           (goto AwaitingAck last-sent-seq message (ThreeAttempts) to-recvr port status)]
+         [ThreeAttempts ()
+          (send status (ErrorClosed))
+          (goto ClosedState)])])
+
+    (define-state (Closing [status (ChannelOf Status)])
+      [write (r)
+        (send r (WriteFailed))
+        (goto Closing status)]
+      [close (c)
+        ;; just ignore this
+        (goto-this-state)]
+      [from-recvr (m)
+        (case m
+          [SynAck () (goto-this-state)]
+          [Ack0 () (goto-this-state)]
+          [Ack1 () (goto-this-state)]
+          [FinAck ()
+            (send status (Closed))
+            (goto ClosedState)])]
+      [(timeout 3)
+        (send status (ErrorClosed))
+        (goto ClosedState)])
+
+    (define-state (ClosedState)
+      [write (r)
+        (send r (WriteFailed))
+        (goto-this-state)]
+      [close (c)
+        (goto-this-state)])
+
+    (begin
+      (send to-recvr (Syn receiver-port from-recvr))
+      (goto SynSent to-recvr receiver-port status)))
+   (list write queued-message close from-recvr)))
+
+(define (Manager to-app nic-registration)
+  (spawn-agent
+   (([connect Connect] [from-net SenderMessage])
+
+    (define-state (Ready [to-app (ChannelOf AppMessage)]
+                         [receivers EntryList])
+      [connect (req)
+        (case req
+          [Connect (to-recvr port status)
+            ;; We use spawn-named-agent as a small hack to allow separate definition of agents; this
+            ;; is effectively equivalent to copy-pasting the the Sender code here into a spawn-agent
+            ;; form
+            (spawn-named-agent (write queued-message close from-recvr)
+              (Sender to-recvr port status)
+              (goto-this-state))])]
+      [from-net (m)
+        ;; We assume that any Syn message does not yet have a corresponding Receiver started
+        (case m
+          [Syn (port to-sender)
+            (spawn-named-agent (from-sender) (Receiver to-sender to-app)
+              (goto Ready to-app (Cons (Entry port from-sender) receivers)))]
+          [Message (port seq content)
+            (goto LookingForReceiver m port receivers to-app receivers)]
+          [Fin (port)
+            (goto LookingForReceiver m port receivers to-app receivers)])])
+
+    (define-state (LookingForReceiver [message SenderMessage]
+                                      [port Nat]
+                                      [remaining EntryList]
+                                      [to-app (ChannelOf AppMessage)]
+                                      [receivers EntryList])
+      [(timeout 0)
+        (case remaining
+          [Null () (goto Ready to-app receivers)]
+          [Cons (entry rest)
+            (case entry
+              [Entry (other-port chan)
+                (case (= other-port port)
+                 [True ()
+                  (send chan message)
+                  (goto Ready to-app receivers)]
+                 [False () (goto LookingForReceiver message rest to-app receivers)])])])])
+
+    (begin
+      (send nic-registration from-net)
+      (goto Ready to-app (Null))))
+
+   (list connect from-net)))
+
+;; ---------------------------------------------------------------------------------------------------
+;; Specifications
+
+;; Defines the behavior of the ABTP manager, from the consumer's point of view
+(define-spec ManagerSpec
+  (channels [connect Connect])
+  (define-state (Always)
+    [connect (Connect * * status) ->
+      (Always)
+      (activ [s SenderSpec (Connecting status)])]))
+
+;; Defines the behavior of an ABTP sender, from the point of view of the application layer
+(define-spec SenderSpec
+  (channels [write (ChannelOf WriteResponse)]
+            [close Unit])
+  (define-state (Connecting [status Status])
+    [unobs ->
+      (Closed) (out [status (ConnectFailed)])]
+    [unobs ->
+      (Connected status)
+      (out
+       [status
+        (Connected (spec-chan self write)
+                   (spec-chan self close))])])
+  (define-state (Connected [status Status])
+    [unobs -> (Closed) (out [status (ErrorClosed)])]
+    [write r -> (Connected status) (out [r (Queued)])]
+    [close * -> (Closing status)])
+  (define-state (Closing [status Status])
+    [write r -> (Closing status) (out [r (WriteFailed)])]
+    [close * -> (Closing status)]
+    [unobs -> (Closed) (out [status (ErrorClosed)])]
+    [unobs -> (Closed) (out [status (Closed)])])
+  (define-state (Closed)
+    [write r -> (Closed) (out [r (WriteFailed)])]
+    [close * -> (Closed)]))
