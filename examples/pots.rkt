62d6c263db88116ec64cea83ac7e6b9d007c0524
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2015-05-29 09:47:45 -0400

    Convert POTS to untyped, single-address version

--- examples/pots.rkt
+++ examples/pots.rkt
@@ -11,302 +11,136 @@
 ;; * regardless of who started the call, the side that hangs up first sends Disconnect to the LIM
 ;; * "A" side is the calling side, "B" side is the called side
 
-(provide
- PotsAgent
-
- OffHook
- OnHook
- Digit
-
- StartRing
- StopRing
- Connect
- Disconnect
- StartTone
- StopTone
-
- Dial
- Ring
- Busy
- Fault
-
- Seize
- Seized
- Rejected
- Cleared
- Answered
-
- GetMoreDigits
- Invalid
- Valid)
-
-;; ---------------------------------------------------------------------------------------------------
-;; Types
-
-(define-variant-type Unit [Unit])
-
-(define-variant-type HardwareEvent
-  [OffHook]
-  [OnHook]
-  [Digit [digit Nat]])
-
-;; Assume that the lim somehow has our address attached to these messages. Perhaps we just layer this
-;; agent inside another that attaches the address as needed.
-(define-variant-type LimCommand
-  (StartRing)
-  (StopRing)
-  (Connect [address (ChannelOf PeerMessage)])
-  (Disconnect [address (ChannelOf PeerMessage)])
-  (StartTone [tone-type ToneType])
-  (StopTone)) ; note: the book didn't list StopTone as a command (p. 184)
-
-(define-variant-type ToneType [Dial] [Ring] [Busy] [Fault])
-
-;; NOTE: names for some of these are different in Ulf's version, and some take an extra Pid parameter
-;; in his version
-(define-variant-type PeerMessage
-  [Seize [peer-channel (ChannelOf PeerMessage)]] ; Ulf's: "request_connection"
-  [Seized] ; Ulf's: "accept"
-  [Rejected] ; Ulf's "reject"
-  [Cleared] ; Ulf's: "cancel"
-  ;; note: the book didn't list Answered as one of the possible messages (pp. 184-185)
-  [Answered]) ; Ulf's: "connect"
-
-(define-variant-type AnalysisRequest
-  [AnalysisRequest [digits DigitList] [response-channel (ChannelOf AnalysisResult)]])
-
-(define-variant-type DigitList
-  [NoDigits]
-  [Cons [first-digit Nat] [remaining-digits DigitList]])
-
-(define-variant-type AnalysisResult
-  [GetMoreDigits]
-  [Invalid]
-  [Valid [peer-channel (ChannelOf PeerMessage)]])
-
-(define-variant-type HaveTone?
-  [HaveTone]
-  [NoTone])
+(provide PotsAgent)
 
 ;; ---------------------------------------------------------------------------------------------------
 
 (define (PotsAgent lim analyzer)
   (spawn-agent
-   (([hw-event HardwareEvent] [from-peer PeerMessage] [analysis-result AnalysisResult])
-
+   (self
     (goto Idle lim analyzer)
 
-    (define-state (Idle [lim (ChannelOf LimCommand)] [analyzer (ChannelOf AnalysisRequest)])
-      [hw-event (e)
-        (case e
-          [OffHook ()
-            (send lim (StartTone (Dial)))
-            (goto GettingFirstDigit lim analyzer)]
-          [OnHook () (goto Idle lim analyzer)]
-          [Digit (d) (goto Idle lim analyzer)])]
-      [from-peer (m)
-        (case m
-          [Seize (peer-chan)
-            (send peer-chan (Seized))
-            (send lim (StartRing))
-            (goto RingingBSide lim analyzer peer-chan)]
-          [Seized () (goto Idle lim analyzer)]
-          [Rejected () (goto Idle lim analyzer)]
-          [Cleared () (goto Idle lim analyzer)]
-          [Answered () (goto Idle lim analyzer)])]
-      [analysis-result (r) (goto Idle lim analyzer)])
+    (define-state (Idle lim analyzer) (m)
+      (match m
+        ['OffHook
+         (send lim (list 'StartTone 'Dial))
+         (goto GettingFirstDigit lim analyzer)]
+        [(list 'Seize peer-chan)
+         (send peer-chan 'Seized)
+         (send lim 'StartRing)
+         (goto RingingBSide lim analyzer peer-chan)]
+        [_ (goto Idle lim analyzer)]))
 
-    (define-state (GettingFirstDigit [lim (ChannelOf LimCommand)]
-                                     [analyzer (ChannelOf AnalysisRequest)])
-      [hw-event (e)
-        (case e
-          [OffHook () (goto GettingFirstDigit lim analyzer)]
-          [OnHook ()
-            (send lim (StopTone))
-            (goto Idle lim analyzer)]
-          [Digit (n)
-            (send lim (StopTone))
-            (send analyzer (AnalysisRequest (Cons n (NoDigits)) analysis-result))
-            (goto WaitOnAnalysis lim analyzer (Cons n (NoDigits)))])]
-      [from-peer (m)
-        (case m
-          [Seize (peer-chan)
-            (send peer-chan (Rejected))
-            (goto GettingFirstDigit lim analyzer)]
-          [Seized () (goto GettingFirstDigit lim analyzer)]
-          [Rejected () (goto GettingFirstDigit lim analyzer)]
-          [Cleared () (goto GettingFirstDigit lim analyzer)]
-          [Answered () (goto GettingFirstDigit lim analyzer)])]
-      [analysis-result (r) (goto GettingFirstDigit lim analyzer)])
+    (define-state (GettingFirstDigit lim analyzer) (m)
+      (match m
+        ['OnHook
+         (send lim 'StopTone)
+         (goto Idle lim analyzer)]
+        [(list 'Digit n)
+         (send lim 'StopTone)
+         (send analyzer (list 'AnalysisRequest (list 'Cons n 'NoDigits) self))
+         (goto WaitOnAnalysis lim analyzer (list 'Cons n 'NoDigits))]
+        [(list 'Seize peer-chan)
+         (send peer-chan 'Rejected)
+         (goto GettingFirstDigit lim analyzer)]
+        [_ (goto GettingFirstDigit lim analyzer)]))
 
-    (define-state (GettingNumber [lim (ChannelOf LimCommand)]
-                                 [analyzer (ChannelOf AnalysisRequest)]
-                                 [number DigitList])
-      [hw-event (e)
-        (case e
-          [OffHook () (goto GettingNumber lim analyzer)]
-          [OnHook () (goto Idle lim analyzer)]
-          [Digit (n)
-            (send analyzer (AnalysisRequest (Cons n number) analysis-result))
-            (goto WaitOnAnalysis lim analyzer (Cons n number))])]
-      [from-peer (m)
-        (case m
-          [Seize (peer-chan)
-            (send peer-chan (Rejected))
-            (goto GettingNumber lim analyzer number)]
-          [Seized () (goto GettingNumber lim analyzer number)]
-          [Rejected () (goto GettingNumber lim analyzer number)]
-          [Cleared () (goto GettingNumber lim analyzer number)]
-          [Answered () (goto GettingNumber lim analyzer number)])]
-      [analysis-result (r) (goto GettingNumber lim analyzer number)])
+    (define-state (GettingNumber lim analyzer number) (m)
+      (match m
+        ['OnHook (goto Idle lim analyzer)]
+        [(list 'Digit n)
+         (send analyzer (list 'AnalysisRequest (list 'Cons n number) self))
+         (goto WaitOnAnalysis lim analyzer (list 'Cons n number))]
+        [(list 'Seize peer-chan)
+         (send peer-chan 'Rejected)
+         (goto GettingNumber lim analyzer number)]
+        [_ (goto GettingNumber lim analyzer number)]))
 
-    (define-state (WaitOnAnalysis [lim (ChannelOf LimCommand)]
-                                  [analyzer (ChannelOf AnalysisRequest)]
-                                  [number DigitList])
-      [hw-event (e)
-        (case e
-          [OffHook () (goto WaitOnAnalysis lim analyzer number)]
-          [OnHook () (goto Idle lim analyzer)]
-          ;; Note: because we don't have selective receive, we throw away any numbers dialed while
-          ;; waiting on the analysis. Ideally we would save them in some sort of stack instead
-          [Digit (n) (goto WaitOnAnalysis lim analyzer number)])]
-      [from-peer (m)
-        (case m
-          [Seize (peer-chan)
-            (send peer-chan (Rejected))
-            (goto WaitOnAnalysis lim analyzer number)]
-          [Seized () (goto WaitOnAnalysis lim analyzer number)]
-          [Rejected () (goto WaitOnAnalysis lim analyzer number)]
-          [Cleared () (goto WaitOnAnalysis lim analyzer number)]
-          [Answered () (goto WaitOnAnalysis lim analyzer number)])]
-      [analysis-result (r)
-        (case r
-          [Invalid ()
-            (send lim (StartTone (Fault)))
-            (goto WaitOnHook lim analyzer (HaveTone))]
-          [Valid (peer-chan)
-            (send peer-chan (Seize from-peer))
-            (goto MakeCallToB lim analyzer peer-chan)]
-          [GetMoreDigits () (goto GettingNumber lim analyzer number)])])
+    (define-state (WaitOnAnalysis lim analyzer number) (m)
+      (match m
+        ['OnHook (goto Idle lim analyzer)]
+        [(list 'Seize peer-chan)
+         (send peer-chan 'Rejected)
+         (goto WaitOnAnalysis lim analyzer number)]
+        ['Invalid
+         (send lim (list 'StartTone 'Fault))
+         (goto WaitOnHook lim analyzer 'HaveTone)]
+        [(list 'Valid peer-chan)
+         (send peer-chan (list 'Seize self))
+         (goto MakeCallToB lim analyzer peer-chan)]
+        ['GetMoreDigits (goto GettingNumber lim analyzer number)]
+        ;; Note: because we don't have selective receive, we throw away any numbers dialed while
+        ;; waiting on the analysis. Ideally we would save them in some sort of stack instead
+        [_ (goto WaitOnAnalysis lim analyzer number)]))
 
     ;; Called "calling_B" in Ulf's version
-    (define-state (MakeCallToB [lim (ChannelOf LimCommand)]
-                               [analyzer (ChannelOf AnalysisRequest)]
-                               [peer (ChannelOf PeerMessage)])
-      [hw-event (e)
-        (case e
-          [OffHook () (goto MakeCallToB lim analyzer peer)]
-          [OnHook () (goto Idle lim analyzer)]
-          [Digit (d) (goto MakeCallToB lim analyzer peer)])]
-      [from-peer (m)
-        (case m
-          [Seize (new-peer)
-            (send new-peer (Rejected))
-            (goto MakeCallToB lim analyzer peer)]
-          [Seized ()
-            (send lim (StartTone (Ring)))
-            (goto RingingASide lim analyzer peer)]
-          [Rejected ()
-            (send lim (StartTone (Busy)))
-            (goto WaitOnHook lim analyzer (HaveTone))]
-          [Cleared () (goto MakeCallToB lim analyzer peer)]
-          [Answered () (MakeCallToB lim analyzer peer)])]
-      [analysis-result (r) (goto GettingFirstDigit lim analyzer)])
+    (define-state (MakeCallToB lim analyzer peer) (m)
+      (match m
+        ['OnHook (goto Idle lim analyzer)]
+        [(list 'Seize new-peer)
+         (send new-peer 'Rejected)
+         (goto MakeCallToB lim analyzer peer)]
+        ['Seized
+         (send lim (list 'StartTone 'Ring))
+         (goto RingingASide lim analyzer peer)]
+        ['Rejected
+         (send lim (list 'StartTone 'Busy))
+         (goto WaitOnHook lim analyzer 'HaveTone)]
+        [_ (MakeCallToB lim analyzer peer)]))
 
-    (define-state (RingingASide [lim (ChannelOf LimCommand)]
-                                [analyzer (ChannelOf AnalysisRequest)]
-                                [peer (ChannelOf PeerMessage)])
-      [from-peer (m)
-        (case m
-          [Seize (new-peer)
-            (send new-peer (Rejected))
-            (goto RingingASide lim analyzer peer)]
-          [Seized () (goto RingingASide lim analyzer peer)]
-          [Rejected () (goto RingingASide lim analyzer peer)]
-          [Cleared () (goto RingingASide lim analyzer peer)]
-          [Answered ()
-            (send lim (StopTone))
-            (send lim (Connect peer))
-            (goto Speech lim analyzer peer)])]
-      [hw-event (e)
-        (case e
-          [OffHook () (goto RingingASide lim analyzer peer)]
-          [OnHook ()
-            (send peer (Cleared))
-            (send lim (StopTone))
-            (goto Idle lim analyzer)]
-          [Digit () (goto RingingASide lim analyzer peer)])]
-      [analysis-result (r) (goto RingingASide lim analyzer peer)])
+    (define-state (RingingASide lim analyzer peer) (m)
+      (match m
+        [(list 'Seize new-peer)
+         (send new-peer 'Rejected)
+         (goto RingingASide lim analyzer peer)]
+        ['Answered
+         (send lim 'StopTone)
+         (send lim (list 'Connect peer))
+         (goto Speech lim analyzer peer)]
+        ['OnHook
+         (send peer 'Cleared)
+         (send lim 'StopTone)
+         (goto Idle lim analyzer)]
+        [_ (goto RingingASide lim analyzer peer)]))
 
     ;; this phone is ringing
-    (define-state (RingingBSide [lim (ChannelOf LimCommand)]
-                                [analyzer (ChannelOf AnalysisRequest)]
-                                [peer (ChannelOf PeerMessage)])
-      [from-peer (m)
-        (case m
-          [Seize (new-peer)
-            (send new-peer (Rejected))
-            (goto RingingBSide lim analyzer peer)]
-          [Cleared ()
-            (send lim StopRing)
+    (define-state (RingingBSide lim analyzer peer) (m)
+      (match m
+        [(list 'Seize new-peer)
+         (send new-peer 'Rejected)
+         (goto RingingBSide lim analyzer peer)]
+        ['Cleared
+         (send lim 'StopRing)
+         (goto Idle lim analyzer)]
+        ['OffHook
+         (send lim 'StopRing)
+         (send peer 'Answered)
+         (goto Speech lim analyzer peer)]
+        [_ (goto RingingBSide lim analyzer peer)]))
+
+    (define-state (Speech lim analyzer peer) (m)
+      (match m
+        [(list 'Seize new-peer)
+         (send new-peer 'Rejected)
+         (goto Speech lim analyzer peer)]
+        ['Cleared (goto WaitOnHook lim analyzer 'NoTone)]
+        ['OnHook
+         (send lim (list 'Disconnect peer))
+         (send peer 'Cleared)
+         (goto Idle lim analyzer)]
+        [_ (goto Speech lim analyzer peer)]))
+
+    (define-state (WaitOnHook lim analyzer have-tone?) (m)
+      (match m
+        [(list 'Seize new-peer)
+         (send new-peer 'Rejected)
+         (goto WaitOnHook lim analyzer have-tone?)]
+        ['OnHook
+         (match have-tone?
+           ['HaveTone
+            (send lim 'StopTone)
             (goto Idle lim analyzer)]
-          [Seized () (goto RingingBSide lim analyzer peer)]
-          [Rejected () (goto RingingBSide lim analyzer peer)]
-          [Answered () (goto RingingBSide lim analyzer peer)])]
-      [hw-event (e)
-        (case e
-          [OffHook ()
-            (send lim (StopRing))
-            (send peer (Answered))
-            (goto Speech lim analyzer peer)]
-          [OnHook () (goto RingingBSide lim analyzer peer)]
-          [Digit (d) (goto RingingBSide lim analyzer peer)])]
-      [analysis-result (r) (goto RingingBSide lim analyzer peer)])
+           ['NoTone (goto Idle lim analyzer)])]
+        [_ (goto WaitOnHook lim analyzer have-tone?)])))
 
-    (define-state (Speech [lim (ChannelOf LimCommand)]
-                          [analyzer (ChannelOf AnalysisRequest)]
-                          [peer (ChannelOf PeerMessage)])
-      [from-peer (m)
-        (case m
-          [Seize (new-peer)
-            (send new-peer (Rejected))
-            (goto Speech lim analyzer peer)]
-          [Seized () (goto Speech lim analyzer peer)]
-          [Rejected () (goto Speech lim analyzer peer)]
-          [Cleared () (goto WaitOnHook lim analyzer (NoTone))]
-          [Answered () (goto Speech lim analyzer peer)])]
-      [hw-event (e)
-        (case e
-          [OffHook () (goto Speech lim analyzer peer)]
-          [OnHook ()
-            (send lim (Disconnect peer))
-            (send peer (Cleared))
-            (goto Idle lim analyzer)]
-          [Digit (d) (goto Speech lim analyzer peer)])]
-      [analysis-result (r) (goto Speech lim analyzer peer)])
-
-    (define-state (WaitOnHook [lim (ChannelOf LimCommand)]
-                              [analyzer (ChannelOf AnalysisRequest)]
-                              [have-tone? HaveTone])
-      [hw-event (e)
-        (case e
-          [OffHook () (goto WaitOnHook lim analyzer have-tone?)]
-          [OnHook ()
-            (case have-tone?
-             [HaveTone ()
-               (send lim (StopTone))
-               (goto Idle lim analyzer)]
-             [NoTone () (goto Idle lim analyzer)])]
-          [Digit (d) (goto WaitOnHook lim analyzer have-tone?)])]
-      [from-peer (m)
-        (case m
-          [Seize (new-peer)
-            (send new-peer (Rejected))
-            (goto WaitOnHook lim analyzer have-tone?)]
-          [Seized ()   (goto WaitOnHook lim analyzer have-tone?)]
-          [Rejected () (goto WaitOnHook lim analyzer have-tone?)]
-          [Cleared ()  (goto WaitOnHook lim analyzer have-tone?)]
-          [Answered () (goto WaitOnHook lim analyzer have-tone?)])]
-      [analysis-result (r) (goto WaitOnHook lim analyzer have-tone?)]))
-
-   (list hw-event from-peer analysis-result)))
+   self))
