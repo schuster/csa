f202e984e85ef3336d8bb610961b4b07c0867a93
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-12-28 16:33:35 -0500

    Add external arguments to csa-run

--- eval.rkt
+++ eval.rkt
@@ -6,9 +6,9 @@
 
 ;; Starts the given program (a quoted version of the "real" program), returning the address for each
 ;; receptionist as multiple values
-(define (csa-run prog)
+(define (csa-run prog . externals)
   ;; Create a new empty namespace that still shares the module registry with this one so that all
   ;; structs, etc. (especially async-channel) are "the same" as those used in csa-run's caller
   (parameterize ([current-namespace (namespace-anchor->empty-namespace outer-module)])
     (namespace-require 'csa)
-    ((eval prog))))
+    (apply (eval prog) externals)))
